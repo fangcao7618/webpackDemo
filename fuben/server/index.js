469f5194b7c98e7fea4ae28dc72051e8a635445a
wangfangcao277 <wangfangcao277@gmail.com.cn>
2019-08-30 18:30:23 +0800

    功能设计

---
+++ fuben/server/index.js
@@ -0,0 +1,50 @@
+if (typeof window === "undefined") {
+    global.window = {};
+}
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const { renderToString } = require("react-dom/server");
+const SSR = require("../dist/search-server");
+const template = fs.readFileSync(
+    path.join(__dirname, "../dist/search.html"),
+    "utf-8"
+);
+const data = require("./data.json.js");
+
+const server = port => {
+    const app = express();
+
+    app.use(express.static("dist"));
+
+    app.get("/search", (req, res) => {
+        const html = renderMarkup(renderToString(SSR));
+        res.status(200).send(html);
+    });
+
+    app.listen(port, () => {
+        console.log("Server is running on port:", port);
+    });
+};
+const renderMarkup = str => {
+    const dataStr = JSON.stringify(data);
+    return template
+        .replace("<!--HTML_PLACEHOLDER-->", str)
+        .replace(
+            "<!--INITIAL_DATA_PLACEHOLDER-->",
+            `<script>window.__initial_data=${dataStr}</script>`
+        );
+    // return `<!DOCTYPE html>
+    //         <html lang="en">
+    //         <head>
+    //             <meta charset="UTF-8">
+    //             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    //             <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    //             <title>Document</title>
+    //         </head>
+    //         <body>
+    //             <div id="root">${str}</div>
+    //         </body>
+    //         </html>`;
+};
+server(process.env.PORT || 3000);
---
+++ fuben/src/index/index.js
@@ -0,0 +1,3 @@
+import { helloworld } from "./helloworld";
+import "../../common";
+document.write(helloworld());
