d771fdc6b967b4a8ff74005ff06fde419b632af1
wangfangcao277 <wangfangcao277@gmail.com.cn>
2019-08-30 11:25:35 +0800

    修改判断环境

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //设置入口文件
-if ((process.env.NODE_ENV = "production")) {
+if (process.env.NODE_ENV === "production") {
     module.exports = require("./dist/large-number.min.js");
 } else {
     module.exports = require("./dist/large-number.js");
