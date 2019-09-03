f6b36ed8a39ae7fa71292da60c942c07cb0865b7
wangfangcao277 <wangfangcao277@gmail.com.cn>
2019-09-03 13:48:01 +0800

    结束测试用例，冒烟测试和单元测试以及持续集成

--- builder-webpack/test/unit/webpack-base-test.js
+++ builder-webpack/test/unit/webpack-base-test.js
@@ -1,15 +1,21 @@
 const assert = require("assert");
 describe("webpack.base.js test case", () => {
     const baseConfig = require("../../lib/webpack.base.js");
-    console.log(baseConfig);
+
     it("entry", () => {
         assert.equal(
-            baseConfig.entry.index,
-            "/Users/fangcao/Documents/study/study_webpack/webpackDemo/builder-webpack/test/smoke/template/src/index/index.js"
+            baseConfig.entry.index.indexOf(
+                "builder-webpack/test/smoke/template/src/index/index.js"
+            ) > -1,
+            true,
+            "不相等"
         );
         assert.equal(
-            baseConfig.entry.search,
-            "/Users/fangcao/Documents/study/study_webpack/webpackDemo/builder-webpack/test/smoke/template/src/search/index.js"
+            baseConfig.entry.search.indexOf(
+                "builder-webpack/test/smoke/template/src/search/index.js"
+            ) > -1,
+            true,
+            "不相等"
         );
     });
 });
